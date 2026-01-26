4d7c90f043de91d4f572759350ab3e09f83af01d
shelina98 <shelaveliu1@gmail.com>
2026-01-26 22:42:45 +0100

    feat: guest navbar finished.

--- src/app/navbar-component/navbar-component.ts
+++ src/app/navbar-component/navbar-component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { MatDividerModule} from '@angular/material/divider'
 import { RouterLink, RouterLinkActive} from '@angular/router'
 
 @Component({
   selector: 'app-navbar-component',
-  imports: [MatToolbarModule, MatButtonModule, RouterLink, RouterLinkActive],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatDividerModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar-component.html',
   styleUrl: './navbar-component.css',
 })
