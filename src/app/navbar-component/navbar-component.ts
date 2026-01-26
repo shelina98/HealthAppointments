3e2d45055f2f28917e4f4507ecea9a31fdab7fb1
shelina98 <shelaveliu1@gmail.com>
2026-01-26 21:42:44 +0100

    feat: Adding logo at the left page of the navbar.

--- src/app/navbar-component/navbar-component.ts
+++ src/app/navbar-component/navbar-component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink, RouterLinkActive} from '@angular/router'
 
 @Component({
   selector: 'app-navbar-component',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar-component.html',
   styleUrl: './navbar-component.css',
 })
