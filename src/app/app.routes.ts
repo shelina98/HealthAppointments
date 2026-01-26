d1be068b1f94c4fba0153b79f6f758ed9f979d05
shelina98 <shelaveliu1@gmail.com>
2026-01-26 16:08:23 +0100

    feat: Add the routes for main componets.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DoctorDashboard } from './components/doctor-dashboard/doctor-dashboard';
+import { PatientDashboard } from './components/patient-dashboard/patient-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:PatientDashboard},
+
+    { path: 'patient-dashboard', component: PatientDashboard },
+  
+    { path: 'doctor-dashboard', component: DoctorDashboard },
+
+    { path: '**', redirectTo: '' }    
+];
