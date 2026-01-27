7ffcea364172f33ced7889532d7ca43f85ab0a8a
shelina98 <shelaveliu1@gmail.com>
2026-01-27 17:57:07 +0100

    clean

--- src/app/components/login-component/login-component.ts
+++ src/app/components/login-component/login-component.ts
@@ -2,14 +2,12 @@ import { Component, inject } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { Firestore, collection} from '@angular/fire/firestore';
 import { AuthService } from '../../auth/auth-service';
 
 @Component({
@@ -27,8 +25,6 @@ import { AuthService } from '../../auth/auth-service';
 export class LoginComponent {
 
   private fb = inject(FormBuilder);
-  private router = inject(Router);
-  private firestore = inject(Firestore);
   private auth = inject(AuthService)
 
 
