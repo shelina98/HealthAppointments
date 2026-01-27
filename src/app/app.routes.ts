6beccf5621e0e1a13945e2664d4eeca7a6aafc2e
shelina98 <shelaveliu1@gmail.com>
2026-01-27 16:12:11 +0100

    fix: Add the route for logincomponent.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DoctorDashboard } from './components/doctor-dashboard/doctor-dashboard';
 import { PatientDashboard } from './components/patient-dashboard/patient-dashboard';
+import { LoginComponent } from './login-component/login-component';
 
 export const routes: Routes = [
     {path:'', component:PatientDashboard},
@@ -8,6 +9,8 @@ export const routes: Routes = [
     { path: 'patient-dashboard', component: PatientDashboard },
   
     { path: 'doctor-dashboard', component: DoctorDashboard },
+
+    { path: 'login', component:LoginComponent},
 
     { path: '**', redirectTo: '' }    
 ];
