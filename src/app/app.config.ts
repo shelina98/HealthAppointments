ecb9d5c40ec93e8decd67c6c4d2de9d9c2487b95
shelina98 <shelaveliu1@gmail.com>
2026-01-27 15:04:44 +0100

    config: Connect the to firebase.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production:false,
+     firebaseConfig : {
+
+  apiKey: "AIzaSyDSA60WlScLYtvwgwuhDJ9yG9Va7JJtQPU",
+  authDomain: "healthappointments-6032c.firebaseapp.com",
+  projectId: "healthappointments-6032c",
+  storageBucket: "healthappointments-6032c.firebasestorage.app",
+  messagingSenderId: "495393194838",
+  appId: "1:495393194838:web:da3d03c5159cc4eed38cf7",
+  measurementId: "G-8W4WT7RHX7"
+}
+
+};
