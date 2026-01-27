8b87d33cb3fd031920dd63abbbf631a84be166f1
shelina98 <shelaveliu1@gmail.com>
2026-01-27 17:36:08 +0100

    feat: Create the login form.

--- src/app/login-component/login-component.ts
+++ src/app/login-component/login-component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Firestore, collection} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [CommonModule, 
+    ReactiveFormsModule, 
+    MatCardModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatIconModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.css',
 })
 export class LoginComponent {
+
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private firestore = inject(Firestore);
+
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
+
+  onSubmit() {
+      console.log('Logging in...', this.loginForm.value);
+      console.log(collection(this.firestore,'users'));
+ }
+
 
 }
