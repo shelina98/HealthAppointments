6192a46c1836f97656f507892f8fa69208914606
shelina98 <shelaveliu1@gmail.com>
2026-01-29 16:13:42 +0100

    feat: Navbar Finished for every role.

--- src/app/components/navbar-component/navbar-component.ts
+++ src/app/components/navbar-component/navbar-component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule} from '@angular/material/icon';
 import { MatDividerModule} from '@angular/material/divider'
 import { RouterLink, RouterLinkActive} from '@angular/router'
+import { AuthService } from '../../auth/auth-service';
+import { UserRole } from '../../models/users';
 
 @Component({
   selector: 'app-navbar-component',
@@ -13,4 +15,13 @@ import { RouterLink, RouterLinkActive} from '@angular/router'
 })
 export class NavbarComponent {
 
+   auth = inject(AuthService);
+
+  isDoctor = this.auth.isDoctor;
+  isPatient = this.auth.isPatient;
+  isLoggedIn = this.auth.isLoggedIn;
 }
+  
+
+
+
