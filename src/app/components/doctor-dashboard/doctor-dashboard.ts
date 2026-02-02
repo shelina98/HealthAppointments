2dc2ada011273f07abfb1cea46f3c97de0ca6316
shelina98 <shelaveliu1@gmail.com>
2026-02-02 14:29:42 +0100

    feat: show the appointments of the doctor in his dashboard.

--- src/app/components/doctor-dashboard/doctor-dashboard.ts
+++ src/app/components/doctor-dashboard/doctor-dashboard.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AppointmentService } from '../../appointment-service';
+import { collection, collectionData, Firestore, query, where } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Appointments } from '../../models/appointments';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatCardModule } from '@angular/material/card';
+import { AuthService } from '../../auth/auth-service';
+
 
 @Component({
   selector: 'app-doctor-dashboard',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './doctor-dashboard.html',
   styleUrl: './doctor-dashboard.css',
 })
 export class DoctorDashboard {
+
+
+  auth = inject(AuthService)
+  appointService = inject(AppointmentService)
+  firestore = inject(Firestore)
+  user = this.auth.user
+
+  // Firestore stream filtered for requested appoint
+  private docs$ = collectionData(
+    query(collection(this.firestore, 'Appointments'),
+     where('status', '==', 'requested'),
+     where('doctorId','==',this.user()?.id)),
+    { idField: 'id' }
+  ) as Observable<Appointments[]>;
+
+  // Signal holding our doctor list
+
+  appoitments = toSignal(this.docs$, { initialValue: [] });
+
+
+  ngOnInit( ){
+    console.log(this.appoitments)
+  }
 
 }
