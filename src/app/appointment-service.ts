dd068ba0b91897960b94451c46a5b657994efbfd
shelina98 <shelaveliu1@gmail.com>
2026-02-02 12:02:41 +0100

    feat: Add the time and date to the book appointment component. and also changed the api to save those info in db.

--- src/app/appointment-service.ts
+++ src/app/appointment-service.ts
@@ -10,14 +10,16 @@ export class AppointmentService {
 
   async createAppointmentRequest(patient: User, doctor: User, appointmentBody: any) {
     const apptCollection = collection(this.firestore, 'Appointments');
-    
+    const formattedDate = appointmentBody.date instanceof Date ? appointmentBody.date.toISOString().split('T')[0] : appointmentBody.date;
     await addDoc(apptCollection, {
       id: apptCollection.id,
       patientId: patient.id,
       patientName: patient.name,
       doctorId: doctor.id,
       docName: doctor.name,
-      symptoms: appointmentBody,
+      symptoms: appointmentBody.symptoms,
+      date: formattedDate,
+      time: appointmentBody.time,
       status: 'requested', // Hardcoded as requested
     }).then(()=> {
       console.log('finish')
--- src/app/components/book-appointment-component/book-appointment-component.ts
+++ src/app/components/book-appointment-component/book-appointment-component.ts
@@ -6,6 +6,9 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule, MatNativeDateModule} from '@angular/material/core'; 
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-book-appointment-component',
@@ -15,7 +18,11 @@ import { MatButtonModule } from '@angular/material/button';
     MatDialogModule, 
     MatFormFieldModule, 
     MatInputModule, 
-    MatButtonModule
+    MatButtonModule,
+    MatSelectModule,    
+    MatOptionModule,   
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   templateUrl: './book-appointment-component.html',
   styleUrl: './book-appointment-component.css',
@@ -31,19 +38,27 @@ export class BookAppointmentComponent {
   data = inject(MAT_DIALOG_DATA);
 
   bookingForm: FormGroup = this.fb.group({
-    symptoms: ['', [Validators.required, Validators.minLength(5)]]
+    symptoms: ['', [Validators.required, Validators.minLength(5)]],
+    date: [new Date(), Validators.required],
+    time: ['', Validators.required]
   });
 
 
+  timeSlots: string[] = [
+    '08:00', '08:30', '09:00', '09:30', 
+    '10:00', '10:30', '11:00', '11:30',
+    '13:00', '13:30', '14:00', '14:30', '15:00', '15:30'
+  ];
+
   async submitRequest() {
     if (this.bookingForm.valid) {
-      const { symptoms } = this.bookingForm.value;
+      const { symptoms, date, time } = this.bookingForm.value;
       
       try {
         await this.apptService.createAppointmentRequest(
           this.data.patient, 
           this.data.doctor, 
-          symptoms
+          {symptoms, date, time}
         );
         this.dialogRef.close(true);
       } catch (error) {
