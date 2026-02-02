afe6b6436519047b51b48df3491eae04f700919d
shelina98 <shelaveliu1@gmail.com>
2026-02-02 20:37:04 +0100

    feat: Use appoitment list in doc-schedule component/

--- src/app/appointment-list-component/appointment-list-component.ts
+++ src/app/appointment-list-component/appointment-list-component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { AuthService } from '../auth/auth-service';
 import { AppointmentService } from '../appointment-service';
 import { collection, collectionData, Firestore, query, where } from '@angular/fire/firestore';
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -27,11 +28,16 @@ export class AppointmentListComponent {
   appointService = inject(AppointmentService)
   firestore = inject(Firestore)
   user = this.auth.user
+  router = inject(Router)
+
+  currentUrl = signal(this.router.url);
+
+  isDoctorSchedule = computed(() => this.currentUrl().includes('/doctor-schedule'));
 
   // Firestore stream filtered for requested appoint
   private docs$ = collectionData(
     query(collection(this.firestore, 'Appointments'),
-     where('status', '==', 'requested'),
+     where('status', '==', this.isDoctorSchedule()? 'scheduled': 'requested'),
      where('doctorId','==',this.user()?.id)),
     { idField: 'id' }
   ) as Observable<Appointments[]>;
@@ -51,6 +57,9 @@ this.appointService.confirmAppointment(id)
   }
   decline(id:string) {
         this.appointService.deleteAppointment(id)
+
+  }
+  startAppointment(id:string) {
 
   }
 }
--- src/app/doc-schedule-component/doc-schedule-component.ts
+++ src/app/doc-schedule-component/doc-schedule-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AppointmentListComponent } from '../appointment-list-component/appointment-list-component';
 
 @Component({
   selector: 'app-doc-schedule-component',
-  imports: [],
+  imports: [AppointmentListComponent],
   templateUrl: './doc-schedule-component.html',
   styleUrl: './doc-schedule-component.css',
 })
