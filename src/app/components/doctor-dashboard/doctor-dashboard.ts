27e5848df40ad66d77a63ead632e6b9fd641c7aa
shelina98 <shelaveliu1@gmail.com>
2026-02-02 15:25:47 +0100

    fix: Import the appointmentsList in doctor dashboard

--- src/app/components/doctor-dashboard/doctor-dashboard.ts
+++ src/app/components/doctor-dashboard/doctor-dashboard.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AppointmentListComponent } from '../../appointment-list-component/appointment-list-component';
 
 
 @Component({
   selector: 'app-doctor-dashboard',
   imports: [
+    AppointmentListComponent
   ],
   templateUrl: './doctor-dashboard.html',
   styleUrl: './doctor-dashboard.css',
