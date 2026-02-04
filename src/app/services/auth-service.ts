ba63153ce93f9dad0429dc9a589b48ae84aad393
shelina98 <shelaveliu1@gmail.com>
2026-02-04 13:55:37 +0100

    fix: Use local storage for not loggin out on refreshing page.

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,76 +1,78 @@
-import { inject, Injectable , signal, computed,} from '@angular/core';
+import { inject, Injectable, signal, computed } from '@angular/core';
 import { Firestore, collection } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { User } from '../models/users';
-import { query, where, limit, getDocs  } from '@angular/fire/firestore';
+import { query, where, limit, getDocs } from '@angular/fire/firestore';
 import { UserService } from './user-service';
-
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-
   private router = inject(Router);
   private userService = inject(UserService);
 
-  private currentUser = signal<User | null>(null);
+  private currentUser = signal<User | null>(this.getStoredUser());
   user = computed(() => this.currentUser());
 
-
   isLoggedIn = computed(() => !!this.currentUser());
-  correctCredentials : boolean = true;
-
+  correctCredentials: boolean = true;
 
   isDoctor = computed(() => this.currentUser()?.role === 'doctor');
   isPatient = computed(() => this.currentUser()?.role === 'patient');
 
-
   setUser(userData: User) {
+    localStorage.setItem('user_data', JSON.stringify(userData));
     this.currentUser.set(userData);
   }
 
   logout() {
     this.currentUser.set(null);
+    localStorage.removeItem('user_data');
     this.router.navigate(['/']);
   }
 
-  async login(credentials: Partial<{
-    email: string | null;
-    password: string | null;
-}>) {
+  private getStoredUser() {
+    const data = localStorage.getItem('user_data');
+    return data ? JSON.parse(data) : null;
+  }
+
+  async login(
+    credentials: Partial<{
+      email: string | null;
+      password: string | null;
+    }>,
+  ) {
     try {
       const allUsers = await this.userService.getUsers();
-      const foundUser = allUsers.find(u => u.email === credentials.email);
+      const foundUser = allUsers.find((u) => u.email === credentials.email);
 
       if (!foundUser) {
-      console.log('User does not exists./ your email maight be wrong')
-      this.correctCredentials = false
-      return;
+        console.log('User does not exists./ your email maight be wrong');
+        this.correctCredentials = false;
+        return;
       }
 
       if (foundUser.password !== credentials.password) {
-              this.correctCredentials = false
+        this.correctCredentials = false;
 
-        console.log('wrong password')
+        console.log('wrong password');
         return;
       }
 
       //succesful login
-      this.correctCredentials = true
+      this.correctCredentials = true;
       this.setUser(foundUser);
-      console.log("Login successful!", foundUser);
-      
+      console.log('Login successful!', foundUser);
+
       //navigation
       if (foundUser.role === 'doctor') {
         this.router.navigate(['/doctor-dashboard']);
       } else {
         this.router.navigate(['/patient-dashboard']);
       }
-
-    } 
-    catch (error) {
-      console.error("Login error:", error);
+    } catch (error) {
+      console.error('Login error:', error);
     }
   }
 }
