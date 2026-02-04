b562fcf8e24e0a82a94395cadc4061944cb87f63
shelina98 <shelaveliu1@gmail.com>
2026-02-04 02:36:18 +0100

    feat: Create Auth guard.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,19 +4,20 @@ import { LoginComponent } from './components/login-component/login-component';
 import { MedicalHistoryComponent } from './patientComponents/medical-history-component/medical-history-component';
 import { DoctorDashboard } from './doctorComponents/doctor-dashboard/doctor-dashboard';
 import { DocScheduleComponent } from './doctorComponents/doc-schedule-component/doc-schedule-component';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-    {path:'', component:PatientDashboard},
+  { path: '', component: PatientDashboard },
 
-    { path: 'patient-dashboard', component: PatientDashboard },
-  
-    { path: 'doctor-dashboard', component: DoctorDashboard },
+  { path: 'patient-dashboard', component: PatientDashboard },
 
-    { path:'doctor-schedule', component: DocScheduleComponent},
+  { path: 'doctor-dashboard', component: DoctorDashboard, canActivate: [authGuard] },
 
-    { path: 'medical-history', component: MedicalHistoryComponent},
+  { path: 'doctor-schedule', component: DocScheduleComponent, canActivate: [authGuard] },
 
-    { path: 'login', component:LoginComponent},
+  { path: 'medical-history', component: MedicalHistoryComponent, canActivate: [authGuard] },
 
-    { path: '**', redirectTo: '' }    
+  { path: 'login', component: LoginComponent },
+
+  { path: '**', redirectTo: '' },
 ];
